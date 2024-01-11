ec5cecb6b9a1ff56d01ca4e0a921b61d970c6eab
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-11 11:36:07 -0300

    creating sections and rendering in pages

--- angular-site/src/app/pages/homepage/homepage.component.ts
+++ angular-site/src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HerosectionComponent } from '../../components/sections/herosection/herosection.component';
+import { CtasectionComponent } from '../../components/sections/ctasection/ctasection.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [HerosectionComponent, CtasectionComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
